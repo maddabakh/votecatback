d0a46ddb7d52c451a4f8f0a1c6116747fd240ded
Abdoul Aziz SY <azizsy88@gmail.com>
2020-09-16 17:18:28 +0200

    fixing problem with Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ var server=express();
 //configuring body-parser
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json());
-
+server.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+  })
 server.get('/',function(req,res){
   res.setHeader('Content-Type','text/html');
   res.status(200).send('<h1> nfjfs </h1>')
